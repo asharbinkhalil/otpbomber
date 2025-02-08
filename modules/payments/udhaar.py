f0647528df78ebb295c75c454980c6e03d0314c9
asharbinkhalil <k201724@nu.edu.pk>
2025-02-08 22:31:49 +0500

    Udhaar

--- modules/payments/udhaar.py
+++ modules/payments/udhaar.py
@@ -9,7 +9,7 @@ async def udhaar(phone, client, out):
     headers = {
         'origin': 'https://web.udhaar.pk',
         'referer': 'https://web.udhaar.pk/SignUp',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+        'user-agent': random.choice(ua["browsers"]["chrome"]),
     }
 
     json_data = {
